1245a39664991aa4c77c1f13e9652f579455f450
willy <willy@gmail.com>
2021-12-02 17:12:48 -0500

    cv v2

--- src/utils/data/proyectos.js
+++ src/utils/data/proyectos.js
@@ -1,6 +1,15 @@
 import portadaEvento from "../../assets/images/eventoJun/portadaEventoJun.PNG";
 import portadaAsistencia from "../../assets/images/asistenciaJun/portadaAsistenciaJun.PNG";
+import img1Asis from "../../assets/images/asistenciaJun/img1.PNG";
+import img2Asis from "../../assets/images/asistenciaJun/img2.PNG";
 import portadaApiEvento from "../../assets/images/apiEvento/portadaApiEvento.PNG";
+import img1 from "../../assets/images/apiEvento/img1.PNG";
+import img2 from "../../assets/images/apiEvento/img2.PNG";
+import img3 from "../../assets/images/apiEvento/img3.PNG";
+import img4 from "../../assets/images/apiEvento/img4.PNG";
+import img5 from "../../assets/images/apiEvento/img5.PNG";
+import img6 from "../../assets/images/apiEvento/img6.PNG";
+import img7 from "../../assets/images/apiEvento/img7.PNG";
 import portadaPortfolio from "../../assets/images/portfolio/portadaPortfolio.PNG";
 
 export const proyectos = [
@@ -11,7 +20,15 @@ export const proyectos = [
       "Aplicación para inscribirse y administrar eventos y/o charlas de una empresa.",
     url: "https://eventos-app.vercel.app",
     portada: portadaEvento,
-    imagenes: {},
+    imagenes: {
+      img1,
+      img2,
+      img3,
+      img4,
+      img5,
+      img6,
+      img7,
+    },
   },
   {
     id: 2,
@@ -19,6 +36,10 @@ export const proyectos = [
     descrip: "Aplicación para marcar asistencia de los eventos inscritos.",
     url: "https://asistencia-client.vercel.app",
     portada: portadaAsistencia,
+    imagenes: {
+      img1: img1Asis,
+      img2: img2Asis,
+    },
   },
   {
     id: 3,
